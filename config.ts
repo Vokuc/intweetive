f5b22d47d44147e495d600b64df704f829980f30
Vokuc <93860771+Vokuc@users.noreply.github.com>
2023-01-12 12:08:40 +0100

    changed the config files

---
+++ config.ts
@@ -0,0 +1,14 @@
+interface ConfigInterface {
+	apiKey: string;
+	apiSecretKey: string;
+	bearerToken: string;
+}
+
+const config: ConfigInterface = {
+	apiKey: "QMM0XuTfB5dw6HuQOdplDN21P",
+	apiSecretKey: "iVPjF1jJEDArrK8h0aW0N8QHiHfvD3CGBrXajXsDhgyVNKOZMf",
+	bearerToken:
+		"AAAAAAAAAAAAAAAAAAAAAFyelAEAAAAANl11Cn5WmEiS0q2vjVeVnpyoNS8%3DoLBkl5FqQ5wYcC2QyulbaTlGyGZw5WkDNctpquSqW5Smwsn6XA",
+};
+
+export default config;
